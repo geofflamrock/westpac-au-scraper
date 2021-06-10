0be431ebef0373ccbac32320c4ee73ef58e6c885
Geoff Lamrock <geoff.lamrock@gmail.com>
2021-06-10 21:42:07 +1000

    Adds debug and timeout to export transactions

--- src/exportTransactions.ts
+++ src/exportTransactions.ts
@@ -1,5 +1,5 @@
 import { Page } from 'puppeteer';
-import { format } from 'date-fns';
+import { format, addMilliseconds } from 'date-fns';
 import path from 'path';
 import tmp from 'tmp';
 import fs from 'fs';
@@ -24,7 +24,14 @@ export const exportTransactions = async (
   accountName: string,
   startDate?: Date,
   endDate?: Date,
-  exportFormat: ExportFormat = ExportFormat.Ofx
+  exportFormat: ExportFormat = ExportFormat.Ofx,
+  options: {
+    debug?: boolean;
+    downloadTimeoutInMs?: number;
+  } = {
+    debug: false,
+    downloadTimeoutInMs: 30000,
+  }
 ): Promise<string> => {
   await page.goto(
     'https://banking.westpac.com.au/secure/banking/reportsandexports/exportparameters/2/'
@@ -32,33 +39,42 @@ export const exportTransactions = async (
 
   if (startDate !== undefined) {
     const startDateFormatted = format(startDate, 'dd/MM/yyyy');
-    console.log(`Setting start date '${startDateFormatted}'`);
+
+    if (options.debug)
+      console.log(`Setting start date '${startDateFormatted}'`);
+
     await page.click('#DateRange_StartDate', { clickCount: 3 });
     await page.type('#DateRange_StartDate', startDateFormatted);
   }
 
   if (endDate !== undefined) {
     const endDateFormatted = format(endDate, 'dd/MM/yyyy');
-    console.log(`Setting end date '${endDateFormatted}'`);
+
+    if (options.debug) console.log(`Setting end date '${endDateFormatted}'`);
+
     await page.click('#DateRange_EndDate', { clickCount: 3 });
     await page.type('#DateRange_EndDate', endDateFormatted);
   }
 
-  console.log(`Selecting account '${accountName}'`);
+  if (options.debug) console.log(`Selecting account '${accountName}'`);
+
   await page.type('#Accounts_1', accountName);
   await page.waitForTimeout(2000);
   await page.waitForSelector('.autosuggest-suggestions:first-child');
   await page.click('.autosuggest-suggestions:first-child');
 
   const fileTypeSelector = getFileTypeSelector(exportFormat);
-  console.log(`Setting export format '${ExportFormat[exportFormat]}'`);
+
+  if (options.debug)
+    console.log(`Setting export format '${ExportFormat[exportFormat]}'`);
+
   await page.waitForTimeout(2000);
   await page.waitForSelector(fileTypeSelector);
   await page.click(fileTypeSelector);
 
   const tempDir = tmp.dirSync();
 
-  console.log(`Exporting transactions to '${tempDir.name}'`);
+  if (options.debug) console.log(`Exporting transactions to '${tempDir.name}'`);
 
   const client = await page.target().createCDPSession();
   await client.send('Page.setDownloadBehavior', {
@@ -69,14 +85,26 @@ export const exportTransactions = async (
 
   let transactionsFile = '';
 
+  const downloadTimeoutTime = addMilliseconds(
+    new Date(),
+    options.downloadTimeoutInMs || 300000 // 5 minutes
+  );
+
   while (true) {
-    console.log(`Checking for downloaded file in '${tempDir.name}'`);
+    if (options.debug)
+      console.log(`Checking for downloaded file in '${tempDir.name}'`);
+
     const downloadDirFiles = fs.readdirSync(tempDir.name);
 
     if (downloadDirFiles.length > 0) {
       transactionsFile = path.join(tempDir.name, downloadDirFiles[0]);
       break;
     }
+
+    if (new Date() > downloadTimeoutTime) {
+      throw new Error('Transaction file download has timed out');
+    }
+
     await page.waitForTimeout(1000);
   }
 
